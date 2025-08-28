a8a9ffe307d48dca5dedca8e26696b3d6ae05554
Sławomir Sulbiński <slawomir.sulbinski@redge.com>
2025-08-28 23:34:27 +0200

    Add fonts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@fontsource/poppins/700.css'
+import '@fontsource/dm-sans/400.css'
+import '@fontsource/dm-sans/500.css'
+import '@fontsource/dm-sans/700.css'
+import '@fontsource/lato/300.css'
+import '@fontsource/lato/400.css'
+import '@fontsource/lato/700.css'
 import './index.css'
+
 const app = createApp(App)
 
 app.use(router)
