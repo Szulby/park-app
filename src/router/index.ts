a278a0d1bd7b615cb50065039535a4073fe44731
Sławomir Sulbiński <slawomir.sulbinski@redge.com>
2025-08-28 23:58:35 +0200

    Add github pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuth } from '@/composables/useAuth'
 import Login from '@/views/LoginView.vue'
 import Home from '@/views/HomeView.vue'
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
