08fbda33cb2ebe5a9bbd1bf6e93c9bbbf916775c
Sławomir Sulbiński <slawomir.sulbinski@redge.com>
2025-08-28 23:36:30 +0200

    Add header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <header
+    class="w-full h-[280px] mx-auto bg-gradient-to-b to-[#FADF99] from-[#FFB034] rounded-b-[16px] overflow-hidden relative"
+  >
+    <key-icon class="absolute right-0 bottom-0" />
+    <div class="flex items-center mt-4.5 ml-5 gap-5">
+      <button>
+        <back-icon class="w-10 h-10 text-[#15215C]" />
+      </button>
+      <h1 class="font-poppins font-bold text-[32px] leading-[40px] text-[#15215C] mt-11">
+        Otwórz<br />bramę
+      </h1>
+    </div>
+  </header>
+</template>
+<script setup lang="ts">
+import backIcon from '../img/back.svg'
+import keyIcon from '../img/key.svg'
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
-  <default-layout></default-layout>
+  <default-layout>
+    <header-component />
+  </default-layout>
 </template>
 <script setup lang="ts">
-import defaultLayout from '../layouts/DefaultLayout.vue'
+import DefaultLayout from '../layouts/DefaultLayout.vue'
+import HeaderComponent from '../components/HeaderComponent.vue'
 </script>
